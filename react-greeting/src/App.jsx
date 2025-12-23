dbb4578a2ead22893b9d80fa1c3da3227800856c
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2025-12-23 13:47:56 +0000

    add greeting functionality
    When the greeting button is clicked the greeting text changes to personalised greeting

--- react-greeting/src/App.jsx
+++ react-greeting/src/App.jsx
@@ -1,14 +1,19 @@
-
+import { useState } from "react";
 
 function App() {
 
+  const [name, setName] = useState("");
+  const [greeting, setGreeting] = useState("Please enter your name.");
 
   return (
     <div>
       <h1>React Greeting App</h1>
-      <input type="text" placeholder="Enter your name" />
-      <button>Greet Me</button>
-      <p id="greeting-text">Please enter your Name</p>
+      <input type="text" 
+      placeholder="Enter your name"
+      value={name}
+      onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setGreeting(`Hello, ${name}. Welcome to the React Greeting App.`)}>Greet Me</button>
+      <p>{greeting}</p>
     </div>
     
   );
